d5881ea77779d097ad93bf1fe1aa2a0089b50670
Eum SeungJu <109807198+sjsjmine129@users.noreply.github.com>
2024-04-11 18:48:44 +0900

    Title: 코드트리 메신저, Time: 363ms, Memory: 32MB, Status: Wrong Answer - CodeTree

--- 240411/코드트리 메신저/codetree-messenger.py
+++ 240411/코드트리 메신저/codetree-messenger.py
@@ -122,7 +122,7 @@ def changePower(index, newPower):
 
     temp = max(powerBefore, newPower)
     now = tree[node.parent]
-    for i in range(temp):
+    for i in range(1,temp+1):
 
         if powerBefore - temp <= 0:
             now.getNoti +=1
