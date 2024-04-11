19813287b10c52977852f6636eda8e070a0351c8
Eum SeungJu <109807198+sjsjmine129@users.noreply.github.com>
2024-04-11 18:05:28 +0900

    Title: 코드트리 메신저, Time: 1890ms, Memory: 39MB, Status: Wrong Answer - CodeTree

--- 240411/코드트리 메신저/codetree-messenger.py
+++ 240411/코드트리 메신저/codetree-messenger.py
@@ -170,7 +170,9 @@ for turn in range(q-1):
     elif order[0] == 500:
         ret = tree[order[1]].getNoti
         if ret == 11 and num==1:
+            print(order[1])
             printTree()
+
         if ret == 11 and num == 0:
             num+=1
         print(tree[order[1]].getNoti)
