85c8e926d184549111288b992120e86aba111da1
Eum SeungJu <109807198+sjsjmine129@users.noreply.github.com>
2024-04-12 17:30:09 +0900

    Title: 코드트리 오마카세, Time: 989ms, Memory: 60MB, Status: Passed - CodeTree

--- 240412/코드트리 오마카세/codetree-omakase.py
+++ 240412/코드트리 오마카세/codetree-omakase.py
@@ -1,9 +1,8 @@
+# *heapq
+# *hashmap
 import heapq
-#회전 -> 놓기 -> 앉기 -> 먹기 -> 사진(사람 수, 초밥수)
-[l, q] = map(int, input().split())
 
-# belt = [{} for _ in range(l)]
-# seat = [['']]*l
+[l, q] = map(int, input().split())
 
 sushiNum = []
 customerNum = []
@@ -28,6 +27,7 @@ def printAll():
 def findBeltInTime(belt, t):
     return (belt + t)%l
 
+#벨트의 시간의 위치
 def findXInTime(x,t):
     belt0 = t%l
     if x >= belt0:
@@ -35,18 +35,20 @@ def findXInTime(x,t):
     else:
         return x + l - belt0 
 
-
+#스시 이벤트 저장
 def makeSushi(name, x, t):
     if name in sushi:
         sushi[name].append([t, findXInTime(x, t)])
     else:
         sushi[name] =[[t, findXInTime(x, t)]]
 
+#손님 이벤트 저장
 def customerCome(name, x, n, t):
     customer[name] = [t, x, n]
 
 
-# main logic
+#### main logic
+#기록
 for i in range(q):
     order = input().split()
 
@@ -59,9 +61,6 @@ for i in range(q):
     #사진 촬영
     elif order[0] == '300':
         photo.append(int(order[1]))
-
-# printAll()
-
 
 ### 손님 들어오고 나가는것 and 초밥 올리고 먹는것 계산
 for name in sushi:
@@ -96,7 +95,7 @@ for name in sushi:
 # print("customer",customerNum)
 # print("sushi", sushiNum)   
 
-
+#출력
 sushiEvent = [0,0]
 customerEvent = [0,0]
 sNum = 0
@@ -115,98 +114,4 @@ for i in photo:
             break
         customerEvent = heapq.heappop(customerNum)  
 
-    print(cNum, sNum) 
-    
-
-
-
-
-
-
-
-########## past code -> out of time
-
-# #회전 -> 놓기 -> 앉기 -> 먹기 -> 사진(사람 수, 초밥수)
-# [l, q] = map(int, input().split())
-
-# belt = [{} for _ in range(l)]
-# seat = [['']]*l
-
-# sushiNum = 0
-# customerNum = 0
-# time = 0
-
-# ###functions
-# # 벨트 위치 찾기
-# def findBelt(x):
-#     offset = time % l
-#     return x-offset
-
-# # 초밥 놓기
-# def makeSushi(name, x):
-#     global sushiNum 
-#     index = findBelt(x)
-
-#     if name in belt[index]:
-#         belt[index][name] = belt[index][name] + 1
-#     else:
-#         belt[index][name] = 1
-#     sushiNum += 1
-
-# # 손님 들어옴
-# def customerCome(name, x, n):
-#     global customerNum 
-
-#     seat[x] = [name, n]
-#     customerNum += 1
-
-# #초밥 먹기
-# def eatSushi():
-#     global customerNum 
-#     global sushiNum 
-
-#     for i in range(l):
-#         person = seat[i]
-#         if person[0] == '': #아무도 없음
-#             continue
-        
-#         index = findBelt(i)
-
-#         if person[0] in belt[index]: # 초밥 있음
-#             afterEat = person[1] - belt[index][person[0]]
-
-#             #벨트에서 초밥 삭제
-#             sushiNum -= belt[index][person[0]]
-#             del belt[index][person[0]]
-
-#             if afterEat == 0: #배부르면 나감
-#                 seat[i] = ['']
-#                 customerNum -= 1
-#             else: #아직이면 그대로
-#                 seat[i][1] = afterEat
-
-
-# # main logic
-# for i in range(q):
-#     order = input().split()
-
-#     #move belt
-#     nextTime =int(order[1])
-#     while time < nextTime:
-#         time += 1
-#         eatSushi()
-
-#     #초밥 만들기
-#     if order[0] == '100':
-#         makeSushi(order[3], int(order[2]))
-
-#     #손님 입장
-#     if order[0] == '200':
-#         customerCome(order[3], int(order[2]), int(order[4]))
-
-#     # 초밥 먹기
-#     eatSushi()
-
-#     #사진 촬영
-#     if order[0] == '300':
-#         print(customerNum, sushiNum)
+    print(cNum, sNum)
