749423757a0cc2977c26474cba1e66fb348b954d
Eum SeungJu <109807198+sjsjmine129@users.noreply.github.com>
2024-09-20 11:10:03 +0900

    Title: 산타의 선물 공장 2, Time: 138ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240920/산타의 선물 공장 2/santa-gift-factory-2.py
+++ 240920/산타의 선물 공장 2/santa-gift-factory-2.py
@@ -87,16 +87,21 @@ def moveAll(src, dst):
         toMove = 1 + lenAdd - floor(belt[dst][length]/2)
     else:
         toMove = floor(lenBefore/2) + lenAdd - floor(belt[dst][length]/2)
+        print("print",toMove)
     newMid = belt[dst][middle]
-    #길이가 0이면 마지막놈 바꾸기
-    
-    if belt[dst][length] == 1:
-        belt[dst][back] = belt[dst][front]
 
     for i in range(toMove):
         newMid = present[newMid][front]
 
     belt[dst][middle] = newMid
+
+    #마지막놈 바꾸기
+    if lenBefore == 0:
+        belt[dst][back] = belt[src][back]
+    # #길이가 0이면 마지막놈 바꾸기
+    # if belt[dst][length] == 1:
+    #     belt[dst][back] = belt[dst][front]
+
 
     # src 벨트 정보 바꾸기
     belt[src] = [-1, -1, 0, -1]
